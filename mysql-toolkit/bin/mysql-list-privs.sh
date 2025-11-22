99798aa1049e43008185ec2a5248bfd606695a4d
Julio <soubsoub@users.noreply.github.com>
2025-11-21 23:58:30 -0500

    Simplify usage message for mysql-list-privs.sh

--- mysql-toolkit/bin/mysql-list-privs.sh
+++ mysql-toolkit/bin/mysql-list-privs.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [ "$#" -lt 1 ] || [ "$#" -gt 2 ]; then
-    echo "Usage: $0 <user> [host]"
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <user>" >&2
     exit 1
 fi
 
 USER="$1"
-HOST="${2:-%}"
-
-mysql -NBe "SHOW GRANTS FOR '${USER}'@'${HOST}';"
+mysql -NBe "SHOW GRANTS FOR '${USER}'@'%';"
